cc1d970c7ad48bbf3b1524217a2e56c645da4251
Linh Võ Trần Nhã <vtrnnhlinh@gmail.com>
2024-12-13 11:03:13 +0700

    update

---
+++ STM32/Lab5/Core/Inc/uart_communication.h
@@ -0,0 +1,20 @@
+/*
+ * uart_communication.h
+ *
+ *  Created on: Dec 13, 2022
+ *      Author: dell
+ */
+
+#ifndef INC_UART_COMMUNICATION_H_
+#define INC_UART_COMMUNICATION_H_
+
+#include "main.h"
+#include "software_timer.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "global.h"
+#include "command_parser.h"
+
+void uart_communiation_fsm(ADC_HandleTypeDef hadc1, UART_HandleTypeDef huart2);
+
+#endif /* INC_UART_COMMUNICATION_H_ */
---
+++ STM32/Lab5/Core/Src/global.c
@@ -0,0 +1,26 @@
+/*
+ * global.c
+ *
+ *  Created on: Dec 7, 2024
+ *      Author: vtrnn
+ */
+#include "global.h"
+
+// State init
+int status_parser = INIT_STR;
+int status_uart = WAIT_RST;
+
+uint8_t temp = 0;
+uint8_t buffer[MAX_BUFFER_SIZE];
+uint8_t index_buffer = 0;
+uint8_t buffer_flag = 0;
+
+uint8_t command[MAX_BUFFER_SIZE];
+uint8_t command_index = 0;
+uint8_t command_flag = 0;
+
+char str[50];
+uint32_t ADC_value = 0;
+
+
+
